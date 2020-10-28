53ded3e4067a67850c3ededfd94afc389c9edd39
AxQus <70443292+AxQus@users.noreply.github.com>
2020-10-28 20:28:10 +0100

    Delete auto_copy.js

--- js/auto_copy.js
+++
@@ -1,8 +0,0 @@
-function auto_copy(text) {
-	var temp = $("<input>");
-	$("body").append(temp);
-	temp.val(text).select();
-	document.execCommand("copy");
-	temp.remove();
-	alert("Skopiowano do twojego schowka!");
-}
